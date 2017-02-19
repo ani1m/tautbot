73b32068ef64c27e6f24a3e5a2651fa9d5aefede
ani1m <ani_mal@videotron.ca>
2017-02-19 13:31:52 -0500

    retest nothing added

--- src/index.js
+++ src/index.js
@@ -73,7 +73,7 @@ function isDefined(obj) {
     return obj != null;
 }
 
-controller.hears(['.*'], ['direct_message', 'direct_mention'], (bot, message) => {
+controller.hears(['.*'], ['direct_message', 'direct_mention'], (bot, message) => { // bot listen methods
     try {
         if (message.type == 'message') {
             if (message.user == bot.identity.id) {
